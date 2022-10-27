9abdb1ba20c2a606e41ba733ce121ceaf0f738b4
Amrelah <amrellahdelil@gmail.com>
2022-10-27 08:34:55 +0300

    next

---
+++ lib/constants.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+const topTitle =
+    TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 25.0);
+
+const cardTitle =
+    TextStyle(color: Colors.grey, fontSize: 15.0, fontWeight: FontWeight.bold);
+
+const kProfileNameStyle = TextStyle(
+  color: Colors.indigo,
+  fontWeight: FontWeight.bold,
+  fontSize: 12.0,
+);
+
+const kHeadingTitleStyle = TextStyle(
+  color: Color(0xFF1A237E), // indigo[900]
+  fontWeight: FontWeight.bold,
+  fontSize: 20.0,
+);
+const kEnabledBorderColor = Color(0xFF9E9E9E); //grey[500]
+const kFocusedBorderSideColor = Color(0xFF1A237E); //indigo[900]
+
+const kForgotPassword = TextStyle(
+  color: Color(0xFF1A237E), //indigo[900]
+  fontWeight: FontWeight.bold,
+);
+
+const kLoginStyle = TextStyle(
+  color: Colors.white,
+  fontWeight: FontWeight.bold,
+  fontSize: 18,
+);
+// You can add your own constants below as well
---
+++ lib/screens/login_page.dart
@@ -0,0 +1,148 @@
+import '/constants.dart';
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  bool _obscureText = true;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Center(
+                child: Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 30.0),
+                    child: Image(
+                      height: 200.0,
+                      width: 200.0,
+                      image: AssetImage(
+                        'images/empowering-life-final.png',
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Text(
+                'Dan Energy Ethiopia',
+                style: kHeadingTitleStyle,
+              ),
+              SizedBox(
+                height: 25.0,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                    contentPadding: EdgeInsets.all(20.0),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: kEnabledBorderColor,
+                      ),
+                      borderRadius: BorderRadius.circular(30), //<-- SEE HERE
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: kFocusedBorderSideColor,
+                      ),
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    hintText: 'Username',
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 15.0,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                    contentPadding: EdgeInsets.all(20.0),
+                    suffixIcon: GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          _obscureText = !_obscureText;
+                        });
+                      },
+                      child: Icon(
+                        _obscureText ? Icons.visibility : Icons.visibility_off,
+                        color: Color(0xFF1A237E),
+                      ),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: kEnabledBorderColor,
+                      ),
+                      borderRadius: BorderRadius.circular(30), //<-- SEE HERE
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: kFocusedBorderSideColor,
+                      ),
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    hintText: 'Password',
+                  ),
+                  obscureText: _obscureText,
+                ),
+              ),
+              SizedBox(
+                height: 15.0,
+              ),
+              GestureDetector(
+                onTap: () {
+                  Navigator.pushNamed(context, '/reset');
+                },
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 40.0),
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      'Forgot Password?',
+                      style: kForgotPassword,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: GestureDetector(
+                  onTap: () {
+                    Navigator.pushNamed(context, '/Dashboard');
+                  },
+                  child: Container(
+                    padding: EdgeInsets.all(20.0),
+                    decoration: BoxDecoration(
+                      color: Colors.indigo[900],
+                      borderRadius: BorderRadius.circular(30.0),
+                    ),
+                    child: Center(
+                      child: Text(
+                        'Login',
+                        style: kLoginStyle,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
